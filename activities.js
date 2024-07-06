8c33b82f18498c6a3dec7b5249cd145c8ec12eaa
Kyle Lawrenson <kylelawrenson99@gmail.com>
2024-07-06 22:11:17 +0100

    finish activity 2

--- activities.js
+++ activities.js
@@ -1,16 +1,34 @@
 // ACTIVITY 01
 
-let person = {
-    myName: "Kyle",
-    age: 24,
-    faveSongs: [
-        "Muse - Hysteria",
-        "Level 42 - Something About You",
-        "ACDC - Back In Black"
-    ],
-    sayHi: function() {
-        console.log("Hi, my name is " + this.myName + "!");
+// let person = {
+//     myName: "Kyle",
+//     age: 24,
+//     faveSongs: [
+//         "Muse - Hysteria",
+//         "Level 42 - Something About You",
+//         "ACDC - Back In Black"
+//     ],
+//     sayHi () {
+//         console.log("Hi, my name is " + this.myName + "!");
+//     }
+// }
+
+// person.sayHi ()
+
+// ACTIVITY 02
+
+let pet = {
+        petName: "Coco",
+        tyoeOfPet: "Dog", 
+        petAge: 4,
+        petColour: "Brown",
+
+    eat () {
+         console.log(this.petName + " is eating steak. ");
+    },
+    drink () { 
+        console.log(this.petName + " is drinking water. ");
     }
 }
 
-person.sayHi ()
+pet.eat (), pet.drink ()
